3f05b4cd05303eaf4dc0cf76f2fe8865f66cb3b3
therawbit <sudarshandevkota22@gmail.com>
2024-12-15 00:15:08 +0545

    flameshot and mako

--- scripts/nepalidate.sh
+++ scripts/nepalidate.sh
@@ -2,6 +2,6 @@
 
 today=$(date +%Y-%m-%d)
 
-nepToday=$(curl -s 'https://www.hamropatro.com/getMethod.php' -X POST --data-raw "actionName=dconverter&datefield=${today}&convert_option=eng_to_nep" | awk -F "<span>|</span>" '{print $2}')
-
-notify-send -u low "Nepali Date:" "${nepToday} " 
+nepToday=$(curl 'https://www.hamropatro.com/getMethod.php' --compressed -X POST --data-raw "actionName=dconverter&datefield=2024-12-14&convert_option=eng_to_nep&state=0.4781698699559769" | awk -F "<span>|</span>" '{print $2}' | tr -d '\n')
+echo $nepToday
+notify-send -u low "Nepali Date:" "$nepToday "
