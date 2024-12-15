6bda244956f1e4c31b207df59d453f8de6051486
therawbit <sudarshandevkota22@gmail.com>
2024-12-15 12:03:43 +0545

    nepali date fixed and mako config bg color changed

--- scripts/nepalidate.sh
+++ scripts/nepalidate.sh
@@ -2,6 +2,5 @@
 
 today=$(date +%Y-%m-%d)
 
-nepToday=$(curl 'https://www.hamropatro.com/getMethod.php' --compressed -X POST --data-raw "actionName=dconverter&datefield=2024-12-14&convert_option=eng_to_nep&state=0.4781698699559769" | awk -F "<span>|</span>" '{print $2}' | tr -d '\n')
-echo $nepToday
+nepToday=$(curl 'https://www.hamropatro.com/getMethod.php' --compressed -X POST --data-raw "actionName=dconverter&datefield=$today&convert_option=eng_to_nep&state=0.4781698699559769" | awk -F "<span>|</span>" '{print $2}' | tr -d '\n')
 notify-send -u low "Nepali Date:" "$nepToday "
