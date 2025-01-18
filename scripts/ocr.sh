fbe9b4246add758801ea4059da9c63a57180f2eb
therawbit <sudarshandevkota22@gmail.com>
2025-01-18 22:05:43 +0545

    ocr ported to flameshot

--- scripts/ocr.sh
+++ scripts/ocr.sh
@@ -2,8 +2,9 @@
 
 # Use slurp to select a region, grim to capture the screenshot, and save it to a temporary file
 screenshot=$(mktemp --suffix=.png)
-grim -g "$(slurp)" "$screenshot"
-
+# grim -g "$(slurp)" "$screenshot"
+export XDG_CURRENT_DESKTOP=sway
+flameshot gui -r >"$screenshot"
 # Run tesseract OCR on the screenshot and store the output in a variable
 text=$(tesseract "$screenshot" -)
 # Copy the recognized text to the clipboard using cliphist
