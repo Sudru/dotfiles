573c2b47da967d3e6cceba100558a6ffaa7629fa
therawbit <sudarshandevkota22@gmail.com>
2025-01-26 18:23:21 +0545

    lsp and neotree fixed

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -15,6 +15,7 @@ return {
 				width = 30,
 				mappings = {
 					["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+					["<C-b>"] = "toggle",
 				},
 			},
 		})
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -84,7 +84,10 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}),
 			}),
 			snippet = {
 				expand = function(args)
